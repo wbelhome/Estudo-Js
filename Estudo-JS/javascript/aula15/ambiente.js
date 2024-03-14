e9c26c5d3b1a423645f226de90329e8ac5806289
Wbelhome <william.belhome99@gmail.com>
2024-03-14 14:04:53 -0300

    segundo commit

--- Estudo-JS/javascript/aula15/ambiente.js
+++ Estudo-JS/javascript/aula15/ambiente.js
@@ -1,6 +1,6 @@
-let num = [8, 9, 3, 5, 7 ]
-num.push(1)
-num.sort()
+let num = [8, 9, 3, 5, 7 ] //criando array
+num.push(1) // puxa o elemento da posição 1
+num.sort() // organiza de forma crescente
 console.log(num)
 console.log(`O vetor tem ${num.length} posições`)
 
--- Estudo-JS/javascript/aula15/ex1.js
+++ Estudo-JS/javascript/aula15/ex1.js
@@ -6,8 +6,10 @@ for(let pos = 0; pos < num.length; pos ++){
 */
 
 let num = [12,1,5, 6, 7, 8]
-num.sort()
+num.sort() // Organiza o array de forma crescente
 
+
+// utiliza para array e objects
 for( let pos in num){
     console.log(`A posição ${pos} tem o valor ${num[pos]}`)
 }
